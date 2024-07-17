97501c1fc52da37b9f843fcf9264ef43b8866e47
SANGARRO <heneg1019@gmail.com>
2024-07-17 13:43:38 -0500

    commit-serv

---
+++ src/app/shared/services/product.service.ts
@@ -0,0 +1,32 @@
+// src/app/product.service.ts
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductService {
+  private apiUrl = 'https://api.escuelajs.co/api/v1/products';
+
+  constructor(private http: HttpClient) {}
+
+  getProducts(): Observable<any> {
+    return this.http.get(this.apiUrl);
+  }
+  getProduct(id: number): Observable<any> {
+    return this.http.get(`${this.apiUrl}/${id}`);
+  }
+
+  createProduct(product: any): Observable<any> {
+    return this.http.post(this.apiUrl, product);
+  }
+
+  updateProduct(id: number, product: any): Observable<any> {
+    return this.http.put(`${this.apiUrl}/${id}`, product);
+  }
+
+  deleteProduct(id: number): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/${id}`);
+  }
+}
---
+++ src/app/shared/services/table.service.ts
@@ -0,0 +1,33 @@
+// src/app/product.service.ts
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TableService {
+  private apiUrl = 'https://api.escuelajs.co/api/v1/products';
+
+  constructor(private http: HttpClient) { }
+
+  getProducts(): Observable<any> {
+    return this.http.get(this.apiUrl);
+  }
+
+  getProduct(id: number): Observable<any> {
+    return this.http.get(`${this.apiUrl}/${id}`);
+  }
+
+  createProduct(product: any): Observable<any> {
+    return this.http.post(this.apiUrl, product);
+  }
+
+  updateProduct(id: number, product: any): Observable<any> {
+    return this.http.put(`${this.apiUrl}/${id}`, product);
+  }
+
+  deleteProduct(id: number): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/${id}`);
+  }
+}
